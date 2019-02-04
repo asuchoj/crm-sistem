6059842f8f29c754cbfd530e67a29457282ddfc6
sharstyuk_a <sharstyuk_a@lwo.by>
2019-02-04 09:41:54 +0300

    lesson 24 is done

--- controllers/position.js
+++ controllers/position.js
@@ -1,15 +1,38 @@
-module.exports.getByCategoryId = function (req, res) {
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
 
+module.exports.getByCategoryId = async function (req, res) {
+  try{
+
+  }catch (e) {
+    errorHandler(res, e)
+  }
 };
 
-module.exports.create = function (req, res) {
-
+module.exports.create = async function (req, res) {
+  try{
+    const positions = await Position.find({
+      category: req.params.categoryId,
+      uers: req.user.id
+    })
+  }catch (e) {
+    errorHandler(res, e)
+  }
+  res.status(200).json(position)
 };
 
-module.exports.remove = function (req, res) {
+module.exports.remove = async function (req, res) {
+  try{
 
+  }catch (e) {
+    errorHandler(res, e)
+  }
 };
 
-module.exports.update = function (req, res) {
+module.exports.update = async function (req, res) {
+  try{
 
+  }catch (e) {
+    errorHandler(res, e)
+  }
 };
