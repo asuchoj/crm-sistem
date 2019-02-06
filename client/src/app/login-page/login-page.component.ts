ffc3e5de7448c7a43ac2a1f0369da11d8a035254
sharstyuk_a <sharstyuk_a@lwo.by>
2019-02-06 18:31:35 +0300

    lesson 35 13:44

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,15 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.sass']
 })
-export class LoginPageComponent implements OnInit {
-
-  constructor() {
-  }
-
-  ngOnInit() {
-  }
-}
+export class LoginPageComponent {}
---
+++ client/src/app/shared/layouts/site-layout/site-layout.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-site-layout',
+  templateUrl: './site-layout.component.html',
+  styleUrls: ['./site-layout.component.sass']
+})
+export class SiteLayoutComponent {}
