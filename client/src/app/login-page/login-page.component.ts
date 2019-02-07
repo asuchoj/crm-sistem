de5a0c6f27d79cc429bd5e932bb7a89e1bc0f33b
sharstyuk_a <sharstyuk_a@lwo.by>
2019-02-07 18:09:03 +0300

    lesson 37 is end - created auth service

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,26 +1,57 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validator, Validators} from "@angular/forms";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+import {AuthService} from "../shared/services/auth.service";
+import {Subscription} from "rxjs";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.sass']
 })
-export class LoginPageComponent implements OnInit{
+export class LoginPageComponent implements OnInit, OnDestroy{
 
   form: FormGroup;
 
-  constructor(private fb: FormBuilder){}
+  aSub: Subscription;
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ){}
 
   ngOnInit(){
     this.form = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if(params["registered"]){
+
+      } else if(params['accessDenied']){
+
+      }
     })
   }
 
-  onSubmit(){
-
+  ngOnDestroy(){
+    if(this.aSub) {
+      this.aSub.unsubscribe();
+    }
   }
 
+  onSubmit(){
+    this.form.disable();
+
+    this.aSub = this.auth.login(this.form.value).subscribe(() => {
+      this.router.navigate(['/overview'])
+      },err => {
+      console.log(err);
+      this.form.enable();
+    })
+  }
 }
