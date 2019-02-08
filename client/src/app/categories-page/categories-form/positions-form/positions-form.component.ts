7df9dfc7e9f21c8b62492fc386ba608ad9a01ea6
sharstyuk_a <sharstyuk_a@lwo.by>
2019-02-08 16:37:47 +0300

    lesson 47 is end

---
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'positions-form',
+  templateUrl: './positions-form.component.html',
+  styleUrls: ['./positions-form.component.scss']
+})
+export class PositionsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
